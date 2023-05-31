dcf5662984a4dc5d70b1f7932f2f942f1d0435eb
Alexandre Athayde <alexandre_asj@hotmail.com>
2023-05-31 17:24:08 -0300

    Messaging With Redis

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "learn"
 include("module-build-restful-web-service")
 include("module-consuming-restful-web-service")
 include("module-uploading-file")
+include("module-messaging-with-redis")
